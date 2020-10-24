3c051730516e55bff6d3b74fb0e6aceda2820dc7
Luis_Mathias <luiscraft15@gmail.com>
2020-10-24 11:21:19 -0300

    Arquivos do mapa

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,11 +1,23 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-22.8526212,-45.2378317], 14);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
+
+const icon = L.icon ({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+const popup = L.popup ({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Exemplo orfanato <a href="orphanage.html?id=1" class="choose-orpanhage"> <img src="./public/images/arrow-white.svg"> </a>')
 
 L
-.marker([51.5, -0.09])
+.marker([-22.8526212,-45.2378317], { icon })
 .addTo(map)
-.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-.openPopup();
+.bindPopup(popup);
