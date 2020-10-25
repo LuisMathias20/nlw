9fbccfb7ba425ab995cd5dec46bc35d07300c709
Luis_Mathias <luiscraft15@gmail.com>
2020-10-25 12:11:07 -0300

    Pagina de orfanato terminada

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -3,7 +3,7 @@ const options = {
     touchZoom: false,
     doubleClickZoom: false,
     scrollWheelZoom: false,
-    zoomScroll: false
+    zoomControl: false
 };
 
 const map = L.map('mapid', options).setView([-22.8526212,-45.2378317], 14);
@@ -14,11 +14,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 const icon = L.icon ({
     iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+    iconAnchor: [29, 68]
+});
 
 L
 .marker([-22.8526212,-45.2378317], { icon })
-.addTo(map)
-.bindPopup(popup);
+.addTo(map);
+
+function selectImage(event) {
+    const button = event.currentTarget;
+    
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removeActiveClass);
+
+    button.classList.add("active");
+
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanage-details > img");
+
+    imageContainer.src = image.src;
+}
+
+function removeActiveClass(button) {
+    button.classList.remove("active");
+}
