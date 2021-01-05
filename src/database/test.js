1a17cbbe00eb4e04ea4f3dbeeb624c92f4a7ad87
Luis Mathias <luiscraft15@gmail.com>
2021-01-05 20:52:45 -0300

    Paginas dinamicas

--- src/database/test.js
+++ src/database/test.js
@@ -3,12 +3,28 @@ const saveOrphanage = require('./saveOrphanage.js');
 
 Database.then(async db => {
     /* insert data in the table */
-    await saveOrphanage(db, )
+    await saveOrphanage(db, {
+        lat: "-22.8526212",
+        lng: "-45.2378317",
+        name: "Exemplo orfanato",
+        about: "EXEMPLO",
+        whatsapp: "(12) 9999-9999", 
+        images: [
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=723&q=80"
+        ].toString(),
+        instructions: "Este é um exemplo de descrição para o orfanato.",
+        opening_hours: "Horário de visita 8h até 18h",
+        open_on_weekends: "1"
+    });
 
     /* read data in the table */
     const selectedOrphanages = await db.all("SELECT * FROM orphanages");
     console.log(selectedOrphanages);
 
+    /* select a specific data from db */
     const orphanage = await db.all('SELECT * FROM orphanages WHERE orp_id = "1"');
     console.log(orphanage);
+
+    /* delete data from table */
+    console.log(await db.run("DELETE FROM orphanages WHERE orp_id = '4'"));
 });
--- src/pages.js
+++ src/pages.js
@@ -1,16 +1,48 @@
-const orphanages_data = require('./database/fakedata.js');
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
     index(req, res) {
         return res.render('index');
     },
 
-    orphanage(req, res) {
-        return res.render('orphanage');
+    async orphanage(req, res) {
+
+        let orp_id = req.query.id;
+
+        try {
+            const db = await Database;
+            const results = await db.all('SELECT * FROM orphanages WHERE orp_id = '+orp_id);
+            
+            const orphanage = results[0];
+
+            orphanage.orp_images = orphanage.orp_images.split(",");
+            orphanage.orp_firstImage = orphanage.orp_images[0]
+
+            if(orphanage.orp_open_on_weekends == "0"){
+                orphanage.orp_open_on_weekends = false;
+            } else {
+                orphanage.orp_open_on_weekends = true;
+            }
+
+            console.log(orphanage.orp_open_on_weekends);
+
+            return res.render('orphanage', {orphanage});
+        } catch (error) {
+            console.log(error);
+            return res.send('Database error while trying to select an specific orphanage');
+        }
     }, 
 
-    orphanages(req, res) {
-        return res.render('orphanages', { orphanages_data });
+    async orphanages(req, res) {
+        try {
+            const db = await Database;
+            const orphanages_data = await db.all("SELECT * FROM orphanages"); 
+            return res.render('orphanages', { orphanages_data });    
+        } catch (error) {
+            console.log(error);
+            return res.send('Database error while trying to select all orphanages');
+        }
     },
 
     createOrphanage(req, res) {
